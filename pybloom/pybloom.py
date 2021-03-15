afe06cb4a2494b11258d9d037556d97f40714ad5
SaltyGrandma <nicholasklogan@gmail.com>
2021-03-15 11:52:14 -0400

    refactor to maintain a majority of backward compatibility.

--- pybloom/pybloom.py
+++ pybloom/pybloom.py
@@ -1,128 +1,97 @@
-#pybloom.py
-#Charles J. Lai
-#October 12, 2013
-
-from pyhash import fnv1a_32
-
 """
+pybloom.py
+Original Author: Charles J. Lai
+October 12, 2013
+
+Updated: Nicholas Logan
+March 14, 2021
+
 See readme for more information. Or check out the wikipedia entry.
 """
 
-#== General bloom filter class implementation ============================
+from fnvhash import fnv1a_32
+
+
+# General bloom filter class implementation
 class BloomFilter:
     """
-    Class: A conceptual implmentation of a bloom filter. 
+    Class: A conceptual implementation of a bloom filter.
 
     Disclaimer: Not a true bloom filter as the bit array in this class takes
-    up more memory than a true array of bits. This implmentation uses a list
-    as an abstraction of a bit array.
-	"""
+    up more memory than a true array of bits. This implementation uses a list
+    as an abstraction of a bit array."""
 
-    #Fields (Hidden)
-    _size = 0
-    _hash_count = 0
-    _bit_array = []
-
-    #Immutable properties
-    @property
-    def size(self):
-        """
-        This is the size of the bloom filter i.e. the number of bits of the
-        filter.
-        """
-        return self._size
-
-    @property
-    def hash_count(self):
-        """
-        This is the number of k hash function in the bloom filter.
-        """
-        return self._hash_count
-
-    @property
-    def bit_array(self):
-        """
-        This is the bit array abstraction of the bloom filter itself.
-        """
-        return self._bit_array
-
-    #Built-in Methods
     def __init__(self, m, k):
         """
         Constructor: Creates a Bloom filter with size m bits and k hash
         functions.
         """
-        self._size = m
-        self._hash_count = k
+        self.size = m
+        self.hash_count = k
         #Initialize an array of 0 bits of size m. Our array is a list.
-        self._bit_array = [0 for bin in range(self.size)]
+        self.bit_array = [0 for _ in range(self.size)]
 
     def add(self, string):
         """
         Procedure: Adds a string or list of strings into the bloom filter.
         """
-        #Case 1: string is a single string literal
-        if type(string) == str:
-            for seed in xrange(self.hash_count):
-                result = int(fnv1a_32(string, seed) % self.size)
-                self.bit_array[result] = 1
-        #Case 2: string is a list of strings
-        if type(string) == list:
-            word_list = string
-            for word in word_list:
-                for seed in xrange(self.hash_count):
-                    result = int(fnv1a_32(word, seed) % self.size)
-                    self.bit_array[result] = 1
-            
+        # Case 1: string is a single string literal
+        if isinstance(string, str):
+            self.add_str(string)
+
+        # Case 2: string is a list of strings
+        if isinstance(string, list):
+            self.add_iter(string)
+
+    def add_str(self, string):
+        for seed in range(self.hash_count):
+            self.bit_array[self.bit_index(string, seed)] = 1
+
+    def add_iter(self, iterator):
+        for word in iterator:
+            self.add_str(word)
+
     def test(self, string):
         """
         Returns: True if the word is probably in the filter and False
         if the word is definitely not in the bloom filter.
         """
-        for seed in xrange(self.hash_count):
-            result = int(fnv1a_32(string, seed) % self.size)
-            if self.bit_array[result] == 0:
-                return False
-        return True
+        return all(self.bit_in_filter(string, seed) for seed in range(self.hash_count))
+
+    def bit_in_filter(self, string, seed):
+        return self.bit_value(string, seed) == 1
+
+    def word_in_filter(self, word):
+        return all(self.bit_in_filter(word, seed) for seed in range(self.hash_count))
+
+    def bit_index(self, string, seed):
+        return int(fnv1a_32(string, seed) % self.size)
+
+    def bit_value(self, string, seed):
+        return self.bit_array[self.bit_index(string, seed)]
 
 
-#== Bloom filter implementations =========================================
+# Bloom filter implementations
 class SpellChecker(BloomFilter):
     """
     An bloom filter implementation of a spell checker.
     """
 
     def __init__(self, m, k, word_list_file):
-        word_list = open(word_list_file).read().lower().split(" ")
-        BloomFilter.__init__(self, m, k)
-        self.add(word_list)
+        super(self, SpellChecker).__init__(m, k)
+        with open(word_list_file) as f:
+            self.add(f.read().lower().split(" "))
 
     def find_mispellings(self, string):
-        #Initialize list of words and empty list of mispelled words
-        mispelled_words = []
-        word_list = string.lower().split(" ")
-        #Test each word in the list of words
-        for word in word_list:
-            for seed in xrange(self.hash_count):
-                result = int(fnv1a_32(word, seed) % self.size)
-                #If a word is hashed to a 0 bit, add it to mispelled_words
-                if self.bit_array[result] == 0:
-                    mispelled_words.append(word)
-                    break;
-        #Return the list of mispelled words
-        return mispelled_words
+        return [word for word in string.lower().split(" ") if not self.word_in_filter(word)]
 
 
-class SheSaidFilter(BloomFilter):
+class SheSaidFilter(SpellChecker):  # Set as SpellChecker to reuse the __init__ method
     """
-    A naive Bloom filter implmentation of a that's what she said checker.
+    A naive Bloom filter implementation of a that's what she said checker.
     """
-    def __init__(self, m, k, words_she_said):
-        word_list = open(words_she_said).read().lower().split(" ")
-        BloomFilter.__init__(self, m, k)
-        self.add(word_list)
 
-    def did_she_say(self, input, SSC = 3):
+    def did_she_say(self, input, SSC=3):
         """
         Returns: True if that's what she said. False if that's not what
         she said.
@@ -131,26 +100,13 @@ class SheSaidFilter(BloomFilter):
         of the filter.
         """
         #Initialize list of words and the she_said_coefficient (SSC)
-        she_said_coefficient = 0
-        word_list = input.lower().split(" ")
+        she_said_coefficient = sum(1 for word in input.lower().split(" ") if self.word_in_filter(word))
 
-        #Test each word in the list of words
-        for word in word_list:
-            word_in_filter = True
-            for seed in xrange(self.hash_count):
-                result = int(fnv1a_32(word, seed) % self.size)
-                #If a word is hashed to a 0 bit, break and test next word
-                if self.bit_array[result] == 0:
-                    word_in_filter = False
-                    break;
-            #If the word is hashed to all 1 bits, increment SSC by 1
-            if word_in_filter:
-                she_said_coefficient += 1
         if she_said_coefficient > SSC:
-            print "That's what she said."
+            print("That's what she said.")
             return True
         else:
-            print "That's not what she said."
+            print("That's not what she said.")
             return False
 
 
